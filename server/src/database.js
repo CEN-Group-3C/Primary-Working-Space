4b7b7e65a126f30ccc7125fb1130ef668ae5e0e2
Isaac Hunter <ikehunter5@gmail.com>
2023-10-29 15:40:33 -0400

    connect database

---
+++ server/src/database.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require("sequelize");
+
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const database = process.env.DB_NAME;
+
+const sequelize = new Sequelize(`postgres://${user}:${password}@${host}:${port}/${database}`);
+
+const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database: ", error);
+  }
+};
+
+module.exports = { sq: sequelize, connectToDatabase };
--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,7 @@
 const server = require('./server.js');
+const db = require('./database.js');
+
+db.connectToDatabase();
 
 server.listen(3000, () => {
   console.log('Server is running on port 3000')
